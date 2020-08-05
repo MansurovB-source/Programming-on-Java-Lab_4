142307d1417a431520faad70d665af2a4d3157c3
Behruz Mansurov <bekha.m2000.bm@gmail.com>
2020-08-06 02:03:57 +0300

    Created CoordinateExceptions

---
+++ story.dunno.history/CoordinateException.java
@@ -0,0 +1,12 @@
+/**
+ * Created by IntelliJ IDEA.
+ * Author: Behruz Mansurov
+ */
+public class CoordinateException extends IllegalArgumentException {
+    public CoordinateException() {
+    }
+
+    public CoordinateException(String s) {
+        super(s);
+    }
+}
